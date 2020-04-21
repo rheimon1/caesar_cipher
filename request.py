ef5504e5716da1fa0ffae23dbca5bc13c42a1383
rheimon1 <wallaceramonns.dev@usp.br>
2020-04-21 17:53:14 -0300

    initial commit

--- request.py
+++ request.py
@@ -7,8 +7,5 @@ url = "https://api.codenation.dev/v1/challenge/dev-ps/generate-data?token=SEU_TO
 # requisition result
 r = requests.get(url).json(object_pairs_hook=OrderedDict)
 
-# pretty print JSON String
-data_formatted = json.dumps(r, indent=2, ensure_ascii=False)
-
 with open('answer.json', 'w', encoding='utf-8') as file:
-    file.write(data_formatted)
+    json.dump(r, file, ensure_ascii=False, indent=2)
